dc468b24bdf92a8f72fd29b8697cfd9f1b2198f3
kallynm <43766958+kallynm@users.noreply.github.com>
2019-01-02 20:57:32 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creates a matrix that can cache its inverse
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- 
+    solve
+  getsolve <- function() m
+  list(set = set,          # gives the name 'set' to the set() function defined above
+       get = get,          # gives the name 'get' to the get() function defined above
+       setsolve = setsolve,  # gives the name 'setmean' to the setmean() function defined above
+       getsolve = getsolve)  # gives the name 'getmean' to the getmean() function defined above
+  #example: aMatrix <- makeMatrix(1:10)
+  #aMatrix$get()                retrieve the value of x
+  #aMatrix$getsolve()           retrieve the value of m, which should be NULL
+  
+  #cacheinverse(aMatrix)           notice mean calculated is mean of 30:50, not 1:10
+  #aMatrix$getsolve()            retrieve it directly, now that it has been cached
 
 }
 
 
-## Write a short comment describing this function
+## computes inverse
 
 cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
